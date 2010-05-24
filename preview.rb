6752bbff6364cc0535d8908e3bbd470052581d66
maetl <me@maetl.net>
2010-05-24 17:27:48 +0200

    filling in elements HTML preview

--- preview.rb
+++ preview.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 
+set :public, File.dirname(__FILE__)
+set :views, Proc.new { File.join(root, "tpl") }
+
 before do
   content_type "text/html", :charset => "utf-8"
 end
